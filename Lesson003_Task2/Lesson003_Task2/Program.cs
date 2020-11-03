4bdc38537a99615ec3334a61ce5f699af44408e0
Олександр Дибач <alex.dybach.2014@i.ua>
2020-11-03 17:30:06 +0200

    Done lesson003 task2

---
+++ Lesson003_Task2/Lesson003_Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson003_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть число :");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+                Console.WriteLine($"Число парне{a}");
+            else
+                Console.WriteLine($"Число непарне{a}");
+
+        }
+    }
+}
